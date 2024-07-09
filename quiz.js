881c9be636dcdfb7d7cf8f77baac7fc22c08029a
Mohammad Echaary <mohammadechaary@gmail.com>
2024-07-09 12:17:00 +0100

    new commit

--- quiz.js
+++ quiz.js
@@ -2,10 +2,11 @@ function checkAnswer() {
   let correctAnswer = "4";
   let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
   let feedback = document.querySelector("#feedback");
-  feedback.textContent =
-    correctAnswer == userAnswer
-      ? "Correct! Well done."
-      : "That's incorrect. Try again!";
+  if (correctAnswer == userAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
 }
 
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
