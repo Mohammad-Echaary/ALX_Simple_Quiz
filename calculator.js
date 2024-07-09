09bad1a39de6d7a89d7b56b099676d82a896b8f7
Mohammad Echaary <mohammadechaary@gmail.com>
2024-07-09 12:26:52 +0100

    new commit

--- calculator.js
+++ calculator.js
@@ -1,31 +1,39 @@
 function add(one, two) {
   return (document.getElementById("calculation-result").textContent =
-    +one + +two);
+    one + two);
 }
 function subtract(one, two) {
   return (document.getElementById("calculation-result").textContent =
-    +one - +two);
+    one - two);
 }
 function multiply(one, two) {
   return (document.getElementById("calculation-result").textContent =
-    +one * +two);
+    one * two);
 }
 function divide(one, two) {
   return (document.getElementById("calculation-result").textContent =
-    +one / +two);
+    one / two);
 }
 let num1 = document.getElementById("number1");
 let num2 = document.getElementById("number2");
 
 document
   .getElementById("add")
-  .addEventListener("click", () => add(num1.value, num2.value));
+  .addEventListener("click", () =>
+    add(parseFloat(num1.value), parseFloat(num2.value))
+  );
 document
   .getElementById("subtract")
-  .addEventListener("click", () => subtract(num1.value, num2.value));
+  .addEventListener("click", () =>
+    subtract(parseFloat(num1.value), parseFloat(num2.value))
+  );
 document
   .getElementById("multiply")
-  .addEventListener("click", () => multiply(num1.value, num2.value));
+  .addEventListener("click", () =>
+    multiply(parseFloat(num1.value), parseFloat(num2.value))
+  );
 document
   .getElementById("divide")
-  .addEventListener("click", () => divide(num1.value, num2.value));
+  .addEventListener("click", () =>
+    divide(parseFloat(num1.value), parseFloat(num2.value))
+  );
